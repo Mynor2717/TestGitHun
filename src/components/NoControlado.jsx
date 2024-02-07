7bcdfa55b5384d8fa6e0d13f305833a9a0da8a9c
node <ratafak15@gmail.com>
2024-02-07 08:28:50 -0500

    Formulario de algunos cambiost

--- src/components/NoControlado.jsx
+++ src/components/NoControlado.jsx
@@ -1,13 +1,24 @@
+import { useRef } from "react";
+
 const NoControlado = () => {
+
+  const form = useRef(null) // useRef Sirve para hacer una referencia al DOM pero no al  verdadero Dom sino al virtual Dom de react
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('me diste click')
+    
+    const data = new FormData(form.current);
+    console.log([...data.entries()]);
+
+    const dataObject = Object.fromEntries([...data.entries()]);
+    console.log(dataObject);
   }
   // document.addEventListener('submit', (evento) => {
   //   evento.preventDefault()
   // } )
     return (
-       <form onSubmit={handleSubmit}>
+       <form onSubmit={handleSubmit} ref={form}>
          <input typeof="text" placeholder="Ingrese ToDO" 
          className="form-control mb-2"
           name="title"/>
